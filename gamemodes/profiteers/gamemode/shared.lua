00ef7798fc40ba3deddc55f5913066e7521ec4ae
TheOnly8Z <888888zombies@gmail.com>
2020-05-30 01:11:41 -0400

    Gamemode info

---
+++ gamemodes/profiteers/gamemode/shared.lua
@@ -0,0 +1,7 @@
+DEFINE_BASECLASS( "gamemode_base" )
+
+GM.Name 	= "Profiteers"
+GM.Author 	= "Team Profiteers"
+GM.Email 	= "888888zombies@gmail.com"
+GM.Website 	= "https://github.com/Team-Profiteers/GMod_Profiteers"
+
