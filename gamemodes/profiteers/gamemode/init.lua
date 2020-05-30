1ad6d5587d648cef244fed344a73d42919b32371
TheOnly8Z <888888zombies@gmail.com>
2020-05-30 10:49:51 -0400

    Some skeleton code for events

--- gamemodes/profiteers/gamemode/init.lua
+++ gamemodes/profiteers/gamemode/init.lua
@@ -1,1 +1,6 @@
 DEFINE_BASECLASS( "gamemode_base" )
+
+include("sv_events.lua")
+include("sh_maps.lua")
+
+AddCSLuaFile("sh_maps.lua")
---
+++ gamemodes/profiteers/gamemode/sh_maps.lua
@@ -0,0 +1,18 @@
+local mapinfo = {
+    ["gm_forkbr"] = {
+        ["default"] = {
+            Vector( -7130.54, -12003.67, -9102.97 ), -- church
+            Vector( -13284.07, -7286.59, -10063.97 ), -- mansion side
+            Vector( -11886.23, -6559.51, -10111.97 ), -- mansion front
+            Vector( -11830.8, -824.18, -10239.97 ), -- warehouse side
+            Vector( -11455.59, 1290.69, -10239.97 ), -- warehouse front
+            Vector( -14489.08, 3258.89, -9997.97 ), -- train 1
+            Vector( -3635.68, 3192.98, -10007.97 ), -- train 2
+            Vector( 1289.12, -4715.76, -9020 ), -- satelite 1
+            Vector( 741.65, -7037.26, -9022.39 ), -- satelite 2
+            Vector( -1358.17, -5806.16, -9027.04 ), -- satelite 3
+        }
+    }
+}
+
+Profiteers_MapInfo = mapinfo[game.GetMap()]
