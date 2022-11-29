3bb8775ca68b0f6ae04a476921f3871e6910bf24
Haodong Mo <mohaodong@gmail.com>
2022-11-29 16:55:22 +1000

    NPCs need loot

--- gamemodes/profiteers/entities/entities/pt_airdrop.lua
+++ gamemodes/profiteers/entities/entities/pt_airdrop.lua
@@ -159,6 +159,25 @@ else
         if self:GetArmed() and self.ParachuteModel then
             SafeRemoveEntity(self.ParachuteModel)
         end
+
+        // create red smoke
+
+        local emitter = ParticleEmitter(self:GetPos())
+
+        local particle = emitter:Add("particles/smokey", self:GetPos() + self:GetUp() * 64)
+        particle:SetVelocity(VectorRand() * 16 + self:GetRight() * 400)
+        particle:SetDieTime(math.Rand(0.5, 1.5))
+        particle:SetStartAlpha(100)
+        particle:SetEndAlpha(0)
+        particle:SetStartSize(0)
+        particle:SetEndSize(50)
+        particle:SetRoll(math.Rand(0, 360))
+        particle:SetRollDelta(math.Rand(-1, 1))
+        particle:SetColor(255, 100, 100)
+        particle:SetGravity(Vector(100, 0, 800) + VectorRand() * 64)
+        particle:SetAirResistance(100)
+
+        emitter:Finish()
     end
 
     function ENT:OnRemove()
