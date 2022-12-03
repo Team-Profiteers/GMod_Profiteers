fc372e2572ecb0fd6a39cdf5319f40d4ba655a2d
TheOnly8Z <888888zombies@gmail.com>
2022-12-02 18:09:37 -0600

    fix bomb marker

--- gamemodes/profiteers/entities/weapons/pt_rocket_marker.lua
+++ gamemodes/profiteers/entities/weapons/pt_rocket_marker.lua
@@ -1,7 +1,9 @@
 AddCSLuaFile()
 
-SWEP.PrintName          = "Rocket Strike Marker"
+SWEP.PrintName          = "CAS Bomber Marker"
 SWEP.Slot               = 5
+
+SWEP.Base = "pt_marker_base"
 
 function SWEP:MarkTarget(pos)
     Profiteers:SpawnAttackerPlane(self:GetOwner(), pos)
